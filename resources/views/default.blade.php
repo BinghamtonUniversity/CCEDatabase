cd571c0598e127032ddca00f37c85ed8a2baec03
Tim Cortesi <tcortesi@gmail.com>
2020-09-14 21:30:41 -0400

    Tweak CSS

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,6 +3,8 @@
 <head>
 <title>@yield('title')</title>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+<!-- google fonts -->
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400,600,700,800|Lora:400,500,600,700">
 <link rel="stylesheet" href="{{url('/assets/css/ccedb.css')}}" type="text/css" />
 <link href="{{url('/assets/css/toastr.min.css')}}" rel="stylesheet">
 <link href="{{url('/assets/css/font-awesome.min.css')}}" rel="stylesheet">
@@ -10,14 +12,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body style="margin-top:120px;">
+<body style="margin-top:119px;">
 
 <!-- New Stuff -->
 <nav class="navbar navbar-fixed-top navbar-default" style="background-color:#004333;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header" style="width:100%;">
-      <a class="navbar-brand" href="{{route('home_page')}}" style="height:60px; padding:7.5px 4.5px;color:white;">
+      <a class="navbar-brand" href="{{route('home_page')}}" style="height:60px;padding:7.5px 4.5px;color:white;font-weight: 400;font-size: 1.3rem;">
         <img src="https://www.binghamton.edu/assets/img/logo/binghamton-university.png" style="width:351px;">
         <span class="hidden-xs" style="font-size: 22px;border-left: 1px solid #fff;margin-left: 10px;padding: 5px 0px 5px 10px;vertical-align: middle;">
           CCE Service Listings
@@ -32,8 +34,8 @@
 </nav>
 <!-- End New Stuff -->
 
-<nav class="navbar navbar-fixed-top navbar-default" role="navigation" style="margin-top:60px;">
-  <div class="container-fluid">
+<nav class="navbar navbar-fixed-top navbar-default" role="navigation" style="margin-top:60px;border:0px;">
+  <div class="container-fluid" style="background-color:rgba(0,90,67,1);">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
